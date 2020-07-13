2a9ff244030b74d4649f8fe0c360f259a7b6575b
Quinn Krug <quinn.krug@gmail.com>
2020-07-13 17:44:57 -0600

    render first React component

--- client/main.js
+++ client/main.js
@@ -1,5 +1,20 @@
 
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './main.html';
+
+
+const App = () => {
+  return (
+    <div>
+      React App 2
+    </div>
+  );
+};
+
+Meteor.startup(() =>{
+  ReactDOM.render(<App />, document.querySelector('.container'));
+});
