c9f533855628d3cf9dcef95903f5ee15b4cfde1a
Quinn Krug <quinn.krug@gmail.com>
2020-07-14 15:46:08 -0600

    use dynamic data for images

--- client/main.js
+++ client/main.js
@@ -12,15 +12,22 @@ import './main.html';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {images: []};
+  }
+
   componentWillMount(){
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
-      .then(response => console.log(response));
+      .then(response => this.setState({ images: response.data.data}));
   }
 
   render() {
+    console.log(this.state.images);
     return (
       <div>
-        <ImageList/>
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
