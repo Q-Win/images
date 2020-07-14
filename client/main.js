b769170abfbb871173c49ba07de6bbfa4c3342db
Quinn Krug <quinn.krug@gmail.com>
2020-07-14 13:51:56 -0600

    move ajax request to componenetWillMount

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import axios from 'axios';
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
 
@@ -11,16 +11,21 @@ import ImageList from './components/image_list';
 import './main.html';
 
 
-const App = () => {
-  return (
-    <div>
-      <ImageList/>
-    </div>
-  );
+class App extends Component {
+  componentWillMount(){
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
+      .then(response => console.log(response));
+  }
+
+  render() {
+    return (
+      <div>
+        <ImageList/>
+      </div>
+    );
+  }
 };
 
 Meteor.startup(() =>{
   ReactDOM.render(<App />, document.querySelector('.container'));
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
-    .then(response => console.log(response));
 });
